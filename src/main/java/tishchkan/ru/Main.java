4e055bdad740ba2b5da22ad2c179fde334016e07
V.Sergeeva <tishchkan@rambler.ru>
2023-12-21 00:14:33 +0300

    conflict_test

--- src/main/java/tishchkan/ru/Main.java
+++ src/main/java/tishchkan/ru/Main.java
@@ -2,6 +2,6 @@ package tishchkan.ru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Victoria!");
+        System.out.println("There is a conflict!!!");
     }
 }
